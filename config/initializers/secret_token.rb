f39024dc8d784cc0b5d4e2dd54cb5fefdeda4bf6
Harold Schreckengost <tug@alleghenycyclery.com>
2013-12-14 19:04:10 -0500

    Initial commit
    User table created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BikeFitBeta::Application.config.secret_key_base = 'f2f9a8a8e718d8c727575f4fef64ac863280fe48a80ff0e7d7ec96254027a95ad476f9f2e373d3d48cc2bc45ab63a4b5742aa82e0805ce0bd83f41ccd3dfc138'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BikeFitBeta::Application.config.session_store :cookie_store, key: '_BikeFitBeta_session'
