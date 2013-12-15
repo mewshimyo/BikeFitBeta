43122b07f8a6286561138f53c653bc2b974a6d7d
Harold Schreckengost <tug@alleghenycyclery.com>
2013-12-14 20:52:40 -0500

    12/14 Evening 2
    Established basic validations on all models, created TODOS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def must_be_signed_in
+  	if !user_signed_in?
+  		# redirect_to('') TODO: find out how to redirect to the previous page
+  	end
+  end
+
+
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,32 @@
 class Ride < ActiveRecord::Base
+
+	#Model Layout
+	#---------------
+	#user_id (integer) links to field on User
+	#date (date) is for date of the ride
+	#distance (float) is for distance of ride (in miles) (optional if time is present)
+	#time (integer) is for distance of ride (in minutes) (optional if distance is present)
+	#elevation (integer) is for elevation gain on ride (in feet) (optional)
+	#strava_link (string) is for a strava link (optional)
+	#type (boolean) is false for road (default) and true for MTB
 	belongs_to :user
+
+	validates :user_id, numericality: { only_integer: true }
+	validates_with NotFuture #Ensure date is not in the future
+	validates_with DistanceOrTimePresent #Ensure either Distance or Time are present
+	validates :elevation, numericality :true
 end
+
+
+class NotFuture < ActiveModel::Validator #Validates entered date is not in the future
+	def validate(record)
+		#code goes here, yo!
+	end
+end
+
+class DistanceOrTimePresent < ActiveModel::Validator #Validates that at least 1 of (distance|time) is present
+	def validate(record)
+		#code goes here, yo!
+	end
+end
+
