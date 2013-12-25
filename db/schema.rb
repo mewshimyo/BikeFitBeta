4f627a3209612d3ee9d66741b2b0b024b9327d7d
Harold Schreckengost <tug@alleghenycyclery.com>
2013-12-25 01:18:22 -0500

    Switched to PostgreSQL for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131225042606) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "profiles", force: true do |t|
     t.integer  "user_id"
     t.string   "first_name"
@@ -50,8 +53,8 @@ ActiveRecord::Schema.define(version: 20131225042606) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "weights", force: true do |t|
     t.integer  "user_id"
