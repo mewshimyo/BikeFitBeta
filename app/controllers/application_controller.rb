039ba856c43ba5b096ccba2546376ab89837fe27
Harold Schreckengost <tug@alleghenycyclery.com>
2013-12-25 17:32:31 -0500

    First bits of UX :3
    CSS, prettying it up, adding a few things for navigation :3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+
   def must_be_signed_in
   	if !user_signed_in?
   		redirect_to :back
   	end
   end
-
-
 end
